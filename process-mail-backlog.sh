c94631ae7e2a685107118aca2b9b9f53a2085a05
Christian Neumann <x-ian@x-ian.net>
2019-11-07 19:10:27 +0100

    changed dirs

--- process-mail-backlog.sh
+++ process-mail-backlog.sh
@@ -3,7 +3,7 @@
 # try to send all net yet send mails from backlog dir in case 1st send wasn't successful
 # keep it in backlog if still not successful
 
-for i in `ls -t /home/mail-backlog/*.sh`; do
+for i in `ls -t /home/pi/mail-backlog/*.sh`; do
 	/bin/bash -x $i
 done
 
