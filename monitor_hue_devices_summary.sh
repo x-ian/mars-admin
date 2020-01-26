8cad2138b6fe3beafba652b81e6ea1702f619bcf
Christian Neumann <x-ian@x-ian.net>
2020-01-26 23:08:09 +0100

    sdf

--- monitor_hue_devices_summary.sh
+++ monitor_hue_devices_summary.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+# ./monitor_hue_devices_summary.sh > /var/www/html/hue
+
+BASEDIR=$(dirname "$(realpath -s "$0")")
+
 FALSE_COUNT=0
 
-cut -d';' -f2 monitor_hue_devices_history.log | sort | uniq | while read -r i; do
-#for i in $(cut -d';' -f2 monitor_hue_devices_history.log | sort | uniq); do
-##  echo "$i"
-  FALSE_COUNT=`grep "$i" monitor_hue_devices_history.log | grep "false" | wc -l`
-  TRUE_COUNT=$(grep "$i" monitor_hue_devices_history.log | grep "true" | wc -l)
+cut -d';' -f2 $BASEDIR/monitor_hue_devices_history.log | sort | uniq | while read -r i; do
+  FALSE_COUNT=`grep "$i" $BASEDIR/monitor_hue_devices_history.log | grep "false" | wc -l`
+  TRUE_COUNT=$(grep "$i" $BASEDIR/monitor_hue_devices_history.log | grep "true" | wc -l)
   RATE=`echo "scale=2; $TRUE_COUNT / ( $TRUE_COUNT + $FALSE_COUNT ) * 100" | bc`
   echo "$i ; availability ; $RATE ; true ; $TRUE_COUNT ; false ; $FALSE_COUNT"
 done
