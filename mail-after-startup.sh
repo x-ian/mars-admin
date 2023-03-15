24ca317800ec15af0ae791da8f7abb331c1979b5
Christian Neumann <x-ian@x-ian.net>
2023-03-15 22:58:33 +0100

    works for raspbian buster

--- mail-after-startup.sh
+++ mail-after-startup.sh
@@ -15,15 +15,19 @@ sleep 60
 
 # RASPBIAN
 # search all first messages after startup plus the line before
-grep -B 1 'x-info="http://www.rsyslog.com"] start' /var/log/syslog | tail -2 > /tmp/mail-after-startup.log
+#grep -B 1 'x-info="http://www.rsyslog.com"] start' /var/log/syslog | tail -2 > /tmp/mail-after-startup.log
 # check if rsyslogd properly exited on signal 15, if not then assume a crash
-grep 'x-info="http://www.rsyslog.com"] exiting on signal 15.' /tmp/mail-after-startup.log
+#grep 'x-info="http://www.rsyslog.com"] exiting on signal 15.' /tmp/mail-after-startup.log
+#CRASH=$?
+
+# NEW RASPBIAN VERSION
+last -Fxn2 shutdown reboot | grep "shutdown"
 CRASH=$?
 
 if [ $CRASH -eq 0 ]; then
 	SUBJECT="`echo "startup at"` `date +%Y%m%d-%H%M`"
 else
-	CRASH_TIME=`head -1 /tmp/mail-after-startup.log | awk -F' ' '{print $2 $1 $3}'`
+	CRASH_TIME="unknown" # `head -1 /tmp/mail-after-startup.log | awk -F' ' '{print $2" "$1" "$3}'`
 	SUBJECT="`echo "startup after crash at"` `date +%Y%m%d-%H%M` `echo " (likely crash time around $CRASH_TIME)"`"
 fi
 BODY="
